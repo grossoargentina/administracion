24ccb3a0d644cb45df62a67a033f7e0f38742420
Ignacio Arnesen <iarnesen@itba.edu.ar>
2026-06-29 10:07:05 -0300

    v1.0

--- supabase/functions/send-whatsapp/index.ts
+++ supabase/functions/send-whatsapp/index.ts
@@ -2,7 +2,7 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 
 const CORS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
 serve(async (req) => {
